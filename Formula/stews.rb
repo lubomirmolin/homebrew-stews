79150ac35e95e41abed70b1df81ab615490dfcea
AutagoCodex <lubomirmolin+codex@gmail.com>
2026-03-03 08:24:10 +0100

    Update stews formula checksum for v0.0.4 tag

--- Formula/stews.rb
+++ Formula/stews.rb
@@ -2,7 +2,7 @@ class Stews < Formula
   desc "Structured Text Editor With Swag"
   homepage "https://github.com/lubomirmolin/stews"
   url "https://github.com/lubomirmolin/stews/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "78b44a41292ff2f93cc1ebd667660ea0fbe70d3f5f6fd00ab4825a8988c38557"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
   version "0.0.4"
 
